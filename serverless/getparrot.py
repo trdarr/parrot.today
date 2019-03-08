7d98dd2cd06caf8e6ca7a093572d546e89ebb582
Aoibhinn Nic Aoidh <oibind@pm.me>
2019-03-08 20:58:35 +0000

    trying to tidy this all up, i clearly don't know how to program though

--- serverless/getparrot.py
+++ serverless/getparrot.py
@@ -3,20 +3,19 @@ import boto3
 import random
 import urllib.request
 
+BUCKET_NAME = "parrot.today"
+LOG = "parrots.log"
+URL_LIST = "parrots.txt"
+IMG = "parrot.jpg"
+
+s3 = boto3.resource("s3")
+
 
 def getparrot(event, context):
+    '''Get the parrot!'''
 
-    BUCKET_NAME = "parrot.today"
-    URL_LIST = "parrots.txt"
-    IMG = "parrot.jpg"
+    # deltemp()
 
-    # delete tmp storage to stop amazon complaining
-    dirPath = "/tmp/"
-    fileList = os.listdir(dirPath)
-    for fileName in fileList:
-        os.remove(dirPath + "/" + fileName)
-
-    s3 = boto3.resource("s3")
     s3.Bucket(BUCKET_NAME).download_file(URL_LIST, "/tmp/" + URL_LIST)
     url = (random.choice(list(open("/tmp/" + URL_LIST))))
     urllib.request.urlretrieve(url, "/tmp/" + IMG)
@@ -26,3 +25,14 @@ def getparrot(event, context):
         ExtraArgs={
             "ContentType": "image/jpeg",
             "ACL": "public-read"})
+
+
+def deltemp():
+    '''The function won't run if tmp stroage is full.
+    Mostly only applicable when testing during
+    development.'''
+
+    dirPath = "/tmp/"
+    fileList = os.listdir(dirPath)
+    for fileName in fileList:
+        os.remove(dirPath + "/" + fileName)
